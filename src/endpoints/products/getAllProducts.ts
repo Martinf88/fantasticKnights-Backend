56ba95860a1f7db59ebcbc1379078145bc484b14
Felipe Fernandez <fettoviktig@gmail.com>
2024-09-25 11:15:50 +0200

    getallproducts refactor for better usage

--- src/endpoints/products/getAllProducts.ts
+++ src/endpoints/products/getAllProducts.ts
@@ -1,41 +1,21 @@
 import { Db, MongoClient, Collection, WithId, FindCursor } from "mongodb";
 import { ProductModel } from "../../models/productModel.js";
 
-async function connect(): Promise<void> {
-    const con: string | undefined = process.env.CONNECTION_STRING
-    if( !con ) {
-        console.log('Error, conection not connecting');
-        return
+const con: string | undefined = process.env.CONNECTION_STRING
+
+async function getAllProducts(): Promise<WithId<ProductModel>[]> {
+    if(!con) {
+        console.log('Connection not connecting bc string');
+        throw new Error('No .env con string')
         
     }
-    let client: MongoClient | null = null
-    try {
-        client = await MongoClient.connect(con)
-        const db: Db = await client.db('fantasticKnights')
-        const col: Collection<ProductModel> = db.collection<ProductModel>('product')
+    const client: MongoClient = await MongoClient.connect(con)
+    const db: Db = await client.db('fantasticKnights')
+    const col: Collection<ProductModel> = db.collection<ProductModel>('product')
 
-        await getAllProducts(col)
-        console.log('precessing getAllProducts');
-        
-    } catch(error) {
+    const result: WithId<ProductModel>[] = await col.find({}).toArray()
+    return result
 
-    } finally {
-        if( client !== null ) {
-            await client.close()
-        }
-    }
 }
 
-async function getAllProducts(col: Collection<ProductModel>): Promise<void> {
-    const cursor: FindCursor<WithId<ProductModel>> = col.find({}).sort({ name: 1 })
-    const product: WithId<ProductModel>[] = await cursor.toArray()
-    console.log('running getallproducts');
-    
-    product.forEach( product => {
-        console.log(`${product.name} kostar ${product.price} euro. \n`);
-        
-
-    })
-}
-
-connect()
+export { getAllProducts }
