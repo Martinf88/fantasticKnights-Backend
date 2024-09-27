bfc1eab37c7cde4dc1f15e37de1b46bcab31f2a7
Stephanie Karlsson <stephanie@ratthjalpen.se>
2024-09-27 11:59:57 +0200

    functions to get products, cart and users from be to fe

--- frontend-src/public/script/api.js
+++ frontend-src/public/script/api.js
@@ -1,4 +1,4 @@
-async function getData() {
+async function getUsers() {
 	const url = "http://localhost:9876"
 
 	try {
@@ -8,13 +8,51 @@ async function getData() {
 		}
 		const data = await response.json()
 		console.log(data);
-		const result = data.result
+		return data;
 	} catch (error) {
 		console.error(error.message);
-		
 	}
+	
 }
 
-export default getData
+getUsers()
 
-getData()
+async function getCart() {
+	const url = "http://localhost:9876"
+
+	try {
+		const response = await fetch(`${url}/cart`)
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`)
+		}
+		const data = await response.json()
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.error(error.message);
+	}
+	
+}
+
+getCart()
+
+async function getProducts() {
+	const url = "http://localhost:9876"
+
+	try {
+		const response = await fetch(`${url}/products`)
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`)
+		}
+		const data = await response.json()
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.error(error.message);
+	}
+	
+}
+
+getProducts()
+
+export {getUsers, getCart, getProducts}
